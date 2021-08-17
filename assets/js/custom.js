02571ed25ebee92d7ce86a29bad1b6c13af8ac27
Kevin Mehta <mehtak248@gmail.com>
2021-08-17 15:14:34 +0530

    Design desktop layout

---
+++ assets/js/custom.js
@@ -0,0 +1,111 @@
+$(document).ready(function() {
+  // Product Thumbnail Slider
+  // $('.slider-for').slick({
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,
+  //   arrows: false,
+  //   fade: true,
+  //   asNavFor: '.slider-nav'
+  // });
+
+  // $('.slider-nav').slick({
+  //   slidesToShow: 3,
+  //   slidesToScroll: 1,
+  //   vertical:true,
+  //   asNavFor: '.slider-for',
+  //   dots: false,
+  //   focusOnSelect: true,
+  //   verticalSwiping:true,
+  //   responsive: [{
+  //     breakpoint: 992,
+  //     settings: {
+  //       vertical: false,
+  //     }
+  //   },
+  //   {
+  //     breakpoint: 768,
+  //     settings: {
+  //       vertical: false,
+  //     }
+  //   },
+  //   {
+  //     breakpoint: 580,
+  //     settings: {
+  //       vertical: false,
+  //       slidesToShow: 3,
+  //     }
+  //   },
+  //   {
+  //     breakpoint: 380,
+  //     settings: {
+  //       vertical: false,
+  //       slidesToShow: 2,
+  //     }
+  //   }
+  //   ]
+  // });
+
+  // $('.logo-slider').slick({
+  //     dots: false,
+  //     autoplay: true,
+  //     arrows: false,
+  //     infinite: false,
+  //     speed: 100,
+  //     slidesToShow: 5,
+  //     slidesToScroll: 1,
+  //     responsive: [
+  //       {
+  //         breakpoint: 1024,
+  //         settings: {
+  //           slidesToShow: 3,
+  //           slidesToScroll: 3,
+  //         }
+  //       },
+  //       {
+  //         breakpoint: 600,
+  //         settings: {
+  //           slidesToShow: 2,
+  //           slidesToScroll: 2
+  //         }
+  //       },
+  //       {
+  //         breakpoint: 480,
+  //         settings: {
+  //           slidesToShow: 1,
+  //           slidesToScroll: 1
+  //         }
+  //       }
+  //     ]
+  // });
+
+  // $(document).scroll(function() {
+  //     if ($(document).scrollTop() >= 25) {
+  //         $('header').addClass('fixed');
+  //         $('nav').addClass('js-scrolling');
+  //     } else {
+  //         $('header').removeClass('fixed');
+  //         $('nav').removeClass('js-scrolling');
+  //     }
+  // });
+
+  // $('#read-more').on('click', function() {
+  //     var txt = $(".other-data").is(':visible') ? 'Read More' : 'Read Less';
+  //     $(this).text(txt);
+  //     $('.other-data').slideToggle();
+  // });
+
+  // $('#list').on('click', function(event) {
+  //   event.preventDefault();
+  //   $(this).toggleClass('active');
+  //   $('#grid').toggleClass('active');
+  //   $('#products .item').addClass('list-group-item');
+  // });
+
+  // $('#grid').on('click', function(event) {
+  //   event.preventDefault();
+  //   $(this).toggleClass('active');
+  //   $('#list').toggleClass('active');
+  //   $('#products .item').removeClass('list-group-item');
+  //   $('#products .item').addClass('grid-group-item');
+  // });
+});
