cde9806301ac0ae4265187426c84ad1aa5074157
Kevin Mehta <mehtak248@gmail.com>
2021-08-19 17:01:17 +0530

    Added slider

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -15,6 +15,37 @@ $(document).ready(function() {
     autoplay: true
   });
 
+  $('.information-slider').slick({
+    arrows: false,
+    autoplay: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
   // Product Thumbnail Slider
   // $('.slider-for').slick({
   //   slidesToShow: 1,
