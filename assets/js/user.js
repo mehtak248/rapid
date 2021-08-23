06cdf27d9e0d1d4b8f30a8ea85c8cea332ca7790
Mehul Padshala <mehulpadshala6@gmail.com>
2021-08-23 17:47:49 +0530

    Added new captcha to form

--- assets/js/user.js
+++ assets/js/user.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
 
+    //Intialize the captcha
+    const captcha = new Captcha($('#RecaptchCanvas'),{
+        length: 4
+    });
+
+    //Refresh the captcha
+    $('.btn-refresh-captch').on('click', function () {
+        captcha.refresh();
+    });
+
     //Validate the contact form
     $("#frm_contact_form").validate({
         rules: {
@@ -11,10 +21,23 @@ $(document).ready(function() {
                 email: true
             },
             security_services: "required",
-            //captcha_verify: "required"
+            captcha_verify: "required"
         },
         submitHandler: function(form) {
+            //check that captcha is valid OR not
+            const captchVal = captcha.valid($('input[name="captcha_verify"]').val());
             
+            if(captchVal == false) {
+                captcha.refresh();
+                $('#captcha_verify_error').html('Please enter valid code.');
+                $('#captcha_verify_error').css('display', 'block');
+                return false;
+            } else {
+                $('#captcha_verify_error').html('');
+                $('#captcha_verify_error').css('display', 'none');
+            }
+
+            //Form Submit Event
             $(form).ajaxSubmit({
                 type: "post",
                 url: 'controllers/contact.php',
@@ -30,12 +53,15 @@ $(document).ready(function() {
 
                     if (response.status == true) {
                         $("#frm_contact_form")[0].reset();
-                        alert('Successfully send');
+                        $('.alert-success').removeClass('d-none');
                     } else {
-                       alert('Error in sending');
+                        //Reload the captcha
+                        captcha.refresh();
+                        $('.alert-danger').removeClass('d-none');
                     }
                 }
             });
         }
     });
+
 });
